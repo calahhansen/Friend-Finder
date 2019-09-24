508d8e107f4d80215596524ced968f563a313730
Calah Hansen <calah.hansen@gmail.com>
2019-09-24 14:51:56 -0500

    added dependencies express and path and started routes but not functioning

---
+++ server.js
@@ -0,0 +1,70 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.port || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    console.log("welcome to friend-finder!")
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+  
+  app.get("/add", function(req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+  });
+  
+  // Displays all people on the friends list
+  app.get("/api/friends", function(req, res) {
+    return res.json(friends);
+  });
+  
+  // Displays a single person, or returns false
+  app.get("/api/friends/:person", function(req, res) {
+    var chosen = req.params.friend;
+  
+    console.log(chosen);
+  
+    for (var i = 0; i < friends.length; i++) {
+      if (chosen === friends[i].routeName) {
+        return res.json(friends[i]);
+      }
+    }
+  
+    return res.json(false);
+  });
+  
+  // Create New Friend - takes in JSON input
+  app.post("/api/friends", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newFriend = req.body;
+    console.log("newFriend", newFriend)
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newFriend);
+  
+    friends.push(newFriend);
+  
+    res.json(newFriend);
+  });
+  
+  // Starts the server to begin listening
+  // =============================================================
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
